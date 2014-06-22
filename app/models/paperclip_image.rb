f4311493fa41a51af818c80de9591692e5930306
Alissa <anaungvi.etc@gmail.com>
2014-06-22 10:19:19 -0400

    Added S3 environment variables to dev, prod, and test.rb in environments folder

--- app/models/paperclip_image.rb
+++ app/models/paperclip_image.rb
@@ -1,12 +1,13 @@
 class PaperclipImage < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :asset, styles: {
-    medium: "300x300>",
-    small: "140x140>",
-    thumb: "60x60!"
-  }
-  
+  has_attached_file :asset,
+                    styles: {
+                      medium: "300x300>",
+                      small: "140x140>",
+                      thumb: "60x60!"
+                      }
+                
   validates_attachment_content_type :asset, :content_type => /\Aimage\/.*\Z/
   
 end
