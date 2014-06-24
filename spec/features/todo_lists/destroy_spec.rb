c29b394c10cf8bb7821847d861bf1a1a9a2a2218
Alissa <anaungvi.etc@gmail.com>
2014-06-24 07:50:19 -0400

    more cosmetic changes

--- spec/features/todo_lists/destroy_spec.rb
+++ spec/features/todo_lists/destroy_spec.rb
@@ -4,10 +4,10 @@ describe "Deleting todo lists" do
   let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list.") }
   
   it "is successful when clicking the destroy link" do
-    visit "/todo_lists"
+    visit "/"
     
     within "#todo_list_#{todo_list.id}" do
-      click_link("Delete To Do List")
+      find("delete-icon").click
     end
     expect(page).to_not have_content(todo_list.title)
     expect(TodoList.count).to eq(0)
